888fb387d7061180b93d45b992a0abb4cb9f9ef2
Danil Kovalev <dannykvlv@gmail.com>
2023-01-31 11:45:55 +0300

    second commit

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -34,7 +34,7 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void testFindLastFilms() {
+    public void testFindLastFilmsLimit() {
         FilmManager manager = new FilmManager(10);
 
         manager.addFilm("Film I");
@@ -44,14 +44,31 @@ public class FilmManagerTest {
         manager.addFilm("Film V");
         manager.addFilm("Film VI");
         manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
 
-        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII"};
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII", "Film VIII", "Film IX", "Film X"};
         String[] actual = manager.findLast();
     }
 
     @Test
     public void testFindLastFilmsLessThanLimit() {
-        FilmManager manager = new FilmManager(0);
+        FilmManager manager = new FilmManager(5);
+
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V"};
+        String[] actual = manager.findLast();
+    }
+
+    @Test
+    public void testFindLastFilmsOverLimit() {
+        FilmManager manager = new FilmManager(10);
 
         manager.addFilm("Film I");
         manager.addFilm("Film II");
@@ -60,8 +77,12 @@ public class FilmManagerTest {
         manager.addFilm("Film V");
         manager.addFilm("Film VI");
         manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
+        manager.addFilm("Film XI");
 
-        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII"};
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII", "Film VIII", "Film IX", "Film X", "Film XI"};
         String[] actual = manager.findLast();
     }
 
