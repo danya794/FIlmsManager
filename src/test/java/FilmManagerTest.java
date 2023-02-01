148a51db92d009399e52f794a9e6ef36544072a1
Danil Kovalev <dannykvlv@gmail.com>
2023-02-01 18:00:58 +0300

    third commit

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -21,6 +21,15 @@ public class FilmManagerTest {
     public void testFindAllFilmsWithoutAdding() {
         FilmManager manager = new FilmManager();
 
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+    }
+
+    @Test
+    public void testFindLastFilmsLimitAuto() {
+        FilmManager manager = new FilmManager();
+
         manager.addFilm("Film I");
         manager.addFilm("Film II");
         manager.addFilm("Film III");
@@ -28,13 +37,16 @@ public class FilmManagerTest {
         manager.addFilm("Film V");
         manager.addFilm("Film VI");
         manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
 
-        String[] expected = {};
-        String[] actual = manager.findAll();
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII", "Film VIII", "Film IX", "Film X"};
+        String[] actual = manager.findLast();
     }
 
     @Test
-    public void testFindLastFilmsLimit() {
+    public void testFindLastFilmsLimitManually() {
         FilmManager manager = new FilmManager(10);
 
         manager.addFilm("Film I");
@@ -53,8 +65,41 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void testFindLastFilmsLessThanLimit() {
-        FilmManager manager = new FilmManager(5);
+    public void testFindLastFilmsOverLimitAuto() {
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+        manager.addFilm("Film VI");
+        manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
+        manager.addFilm("Film XI");
+
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII", "Film VIII", "Film IX", "Film X","Film XI"};
+        String[] actual = manager.findLast();
+    }
+
+    @Test
+    public void testFindLastFilmsLessThanLimitManually() {
+        FilmManager manager = new FilmManager(10);
+
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V"};
+        String[] actual = manager.findLast();
+    }
+    @Test
+    public void testFindLastFilmsLessThanLimitAuto() {
+        FilmManager manager = new FilmManager();
 
         manager.addFilm("Film I");
         manager.addFilm("Film II");
@@ -67,7 +112,7 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void testFindLastFilmsOverLimit() {
+    public void testFindLastFilmsOverLimitManually() {
         FilmManager manager = new FilmManager(10);
 
         manager.addFilm("Film I");
@@ -83,6 +128,16 @@ public class FilmManagerTest {
         manager.addFilm("Film XI");
 
         String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII", "Film VIII", "Film IX", "Film X", "Film XI"};
+        String[] actual = manager.findLast();
+    }
+
+    @Test
+    public void testFindLastFilmsWithoutAddingManually() {
+        FilmManager manager = new FilmManager(10);
+
+
+
+        String[] expected = {};
         String[] actual = manager.findLast();
     }
 
